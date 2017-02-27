4ddd3660f0da9d35dad6f00be859d20893ec291f
Eric Thibeault <thibeault@gmail.com>
2017-02-27 13:36:25 -0700

    Now levraging BOTO3 to get VPC_ID, Security Groups, AZ and subnets base on few filter from the config file

--- ECS/ecs.py
+++ ECS/ecs.py
@@ -9,30 +9,43 @@ from troposphere.ecs import Cluster
 from troposphere.cloudwatch import Alarm, MetricDimension
 from troposphere.autoscaling import LaunchConfiguration
 from troposphere.autoscaling import AutoScalingGroup, Metadata, ScalingPolicy
+from fonctions import readConfigFile
+from fonctions import getVPC, getSecurityGroups, getSubnets
 
-from data import  ClusterInfo
+# Loading the config file based on the argument passed
+data = readConfigFile('filename')
 
-## move config in json
-## with Boto, get VPC, & Private Subnets with list of AZs
-## with boto get SecurityGroups under VPC, find one with tag X if Tag X is not null
+
+## With Boto, get VPC, & Private Subnets with list of AZs
+vpc = getVPC(data)
+
+### Array to use bellow
+AutoScalingGroupSubnets = []
+AutoScalingGroupAvailabilityZones = []
+SecurityGroups = []
+
+## get all security group from VPC matching filter from config
+securityGroups = getSecurityGroups(vpc, data)
+
+for sg in securityGroups:
+    SecurityGroups.append(sg.group_id)
+
+### getting subnets
+subnets = getSubnets(vpc, data)
+
+for subnet in subnets:
+    AutoScalingGroupSubnets.append(subnet.id)
+    AutoScalingGroupAvailabilityZones.append(subnet.availability_zone)
+
+#print(str(AutoScalingGroupSubnets))
+#print(str(AutoScalingGroupAvailabilityZones))
+#print(str(SecurityGroups))
 
 
 t = Template()
 t.add_version('2010-09-09')
 
-t.add_mapping('RegionMap', {
-    "us-east-1": {"AMI": "ami-6df8fe7a"},
-    "us-east-2": {"AMI": "ami-c6b5efa3"},
-    "us-west-1": {"AMI": "ami-1eda8d7e"},
-    "us-west-2": {"AMI": "ami-a2ca61c2"},
-    "eu-west-1": {"AMI": "ami-ba346ec9"},
-    "eu-west-2": {"AMI": "ami-42c5cf26"},
-    "eu-central-1": {"AMI": "ami-e012d48f"},
-    "ap-northeast-1": {"AMI": "ami-08f7956f"},
-    "ap-southeast-1": {"AMI": "ami-f4832f97"},
-    "ap-southeast-2": {"AMI": "ami-774b7314"},
-    "ca-central-1": {"AMI": "ami-be45f7da"}
-})
+t.add_mapping('RegionMap', data["RegionMap"])
 
 
 t.add_description("""\
@@ -43,7 +56,7 @@ AWS CloudFormation ECS Cluster\
 ecsClusterName_param = t.add_parameter(Parameter(
     "ECSClusterName",
     Description="ECS Cluster Name",
-    Default=ClusterInfo['Name'],
+    Default=data['ClusterInfo']['Name'],
     Type="String"
 ))
 
@@ -197,23 +210,23 @@ ContainerInstances = t.add_resource(LaunchConfiguration(
                           Ref('AWS::Region'),
                           '\n'])),
     ImageId=FindInMap("RegionMap", Ref("AWS::Region"), "AMI"),
-    KeyName=ClusterInfo['KeyName'],
-    SecurityGroups=ClusterInfo['SecurityGroups'],
+    KeyName=data['ClusterInfo']['KeyName'],
+    SecurityGroups=SecurityGroups,
     IamInstanceProfile=Ref('EC2InstanceProfile'),
-    InstanceType=ClusterInfo['EC2InstanceType'],
-    AssociatePublicIpAddress=ClusterInfo['AssociatePublicIpAddress'],
-    InstanceMonitoring=ClusterInfo['DetailMonitoring'],
+    InstanceType=data['ClusterInfo']['EC2InstanceType'],
+    AssociatePublicIpAddress=data['ClusterInfo']['AssociatePublicIpAddress'],
+    InstanceMonitoring=data['ClusterInfo']['DetailMonitoring'],
 
 ))
 
 # Amazon EC2 Auto Scaling Group
 ECSAutoScalingGroup = t.add_resource(AutoScalingGroup(
     'ECSAutoScalingGroup',
-    DesiredCapacity=ClusterInfo['DesiredCapacity'],
-    MinSize=ClusterInfo['MinSize'],
-    MaxSize=ClusterInfo['MaxSize'],
-    VPCZoneIdentifier=ClusterInfo['AutoScalingGroupSubnets'],
-    #AvailabilityZones=ClusterInfo['AutoScalingGroupAvailabilityZones'],
+    DesiredCapacity=data['ClusterInfo']['DesiredCapacity'],
+    MinSize=data['ClusterInfo']['MinSize'],
+    MaxSize=data['ClusterInfo']['MaxSize'],
+    VPCZoneIdentifier=AutoScalingGroupSubnets,
+    AvailabilityZones=AutoScalingGroupAvailabilityZones,
     LaunchConfigurationName=Ref('ContainerInstances'),
     Tags=[{'Key':'Name1','Value':'MyAutoScalingGroup','PropagateAtLaunch':'false'},
          {'Key':'Name','Value':'ECSInstancesEC2','PropagateAtLaunch':'true'}],
@@ -250,3 +263,4 @@ EC2CPUScalingPolicy = t.add_resource(ScalingPolicy(
 
 
 print(t.to_json())
+#print("OK!!!")
